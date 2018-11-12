311282a144a29ca937ddbbdc832166caee77cb3d
Michelle <m.rao3@lse.ac.uk>
2018-11-12 19:49:19 +0000

    My first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+print('Hello, World!')
+print(5-6)
+print(8*9)
+print(6/2)
+print("Bob" *3)
+print ("hello".upper())
+print ("the lord of the rings".title())
+
+
+age =10
+like = "painting"
+age_description = "My age is {0} and I like {1}".format(age, like)
+print (age_description)
